1bbaae3a7ffb48a75c375cfbf41238c14ebf85d0
Maciej <m.kamieniak@softflix.com>
2022-04-03 20:43:55 +0200

    added extension methods for dependencyinjection

--- src/Inoxie.Tools.ApiServices/DI/ApiServicesDependencyInjection.cs
+++ src/Inoxie.Tools.ApiServices/DI/ApiServicesDependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using Inoxie.Tools.ApiServices.Abstractions.Interfaces;
 using Inoxie.Tools.ApiServices.Services;
+using Inoxie.Tools.Core.Repository.Abstractions;
+using Inoxie.Tools.DataProcessor.Abstractions.Models;
 using Inoxie.Tools.DataProcessor.DI;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,5 +21,25 @@ public static class ApiServicesDependencyInjectionExtenstions
     public static void AddApiServices(this IServiceCollection services)
     {
         ApiServicesDependencyInjection.Configure(services);
+    }
+
+    public static void AddWriteService<TEntity, TInDto>(this IServiceCollection services)
+        where TEntity : IDataEntity
+    {
+        services.AddScoped<IWriteService<TInDto>, WriteService<TEntity, TInDto>>();
+    }
+
+    public static void AddReadService<TEntity, TOutDto>(this IServiceCollection services)
+        where TEntity : IDataEntity
+    {
+        services.AddScoped<IReadService<TOutDto>, ReadService<TEntity, TOutDto>>();
+    }
+
+    public static void AddFilteredReadService<TEntity, TOutDto, TFilter>(this IServiceCollection services)
+        where TEntity : class, IDataEntity
+        where TFilter : BaseFilterModel
+        where TOutDto : class
+    {
+        services.AddScoped<IFilterReadService<TOutDto, TFilter>, FilteredReadService<TEntity, TOutDto, TFilter>>();
     }
 }
