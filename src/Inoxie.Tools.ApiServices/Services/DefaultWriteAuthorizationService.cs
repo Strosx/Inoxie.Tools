7d91469f563a6f044f719872428b1362ad220b36
Maciej <m.kamieniak@softflix.com>
2022-04-04 16:58:19 +0200

    fix

--- src/Inoxie.Tools.ApiServices/Services/DefaultWriteAuthorizationService.cs
+++ src/Inoxie.Tools.ApiServices/Services/DefaultWriteAuthorizationService.cs
@@ -1,12 +1,10 @@
 ﻿using Inoxie.Tools.ApiServices.Abstractions.Interfaces;
-using Inoxie.Tools.Core.Repository.Abstractions;
 
 namespace Inoxie.Tools.ApiServices.Services;
 
-internal class DefaultWriteAuthorizationService<TEntity> : IWriteAuthorizationService<TEntity>
-    where TEntity : IDataEntity
+internal class DefaultWriteAuthorizationService<TInDto> : IWriteAuthorizationService<TInDto>
 {
-    public Task<bool> AuthorizeAsync(TEntity dto) => Task.FromResult(true);
+    public Task<bool> AuthorizeAsync(TInDto dto) => Task.FromResult(true);
 
     public Task<bool> AuthorizeDeleteAsync(Guid id) => Task.FromResult(true);
 
