35e33f23f69b3d433b129a9667da7d83e3c34489
Alex <aleksander.gorka@gmail.com>
2022-05-03 21:06:22 +0200

    add stringid repositories

---
+++ src/Inoxie.Tools.Core.Repository.StringId.Abstractions/IWriteRepository.cs
@@ -0,0 +1,7 @@
+﻿using Inoxie.Tools.Core.Repository.Abstractions;
+
+namespace Inoxie.Tools.Core.Repository.StringId.Abstractions;
+
+public interface IWriteRepository<in TEntity> : IWriteRepository<TEntity, string> where TEntity : IDataEntity<string>
+{
+}
